6393bb51969f05796a4f28e90049580647d5e210
yunji <yunji378mm@naver.com>
2025-03-26 00:22:00 +0900

    feat: apply update
    update되면 unity에 해당 사항 반영

--- LargeDataProject/Assets/Scripts/SocketClient.cs
+++ LargeDataProject/Assets/Scripts/SocketClient.cs
@@ -20,41 +20,58 @@ public class SocketManager : MonoBehaviour
 
         client.On("cube_updated", response =>
         {
-            CubeData updatedCube = response.GetValue<CubeData>();
-            Debug.Log("수정 "+updatedCube.seq+"="+updatedCube.column5);
+            Debug.Log("resoponse="+response);
+            // 원본 문자열에서 배열 대괄호 제거 후 객체로 변환
+            string jsonString = response.ToString().TrimStart('[').TrimEnd(']');
 
+            // JSON 파싱으로 객체 변환
+            CubeData updatedCube = JsonUtility.FromJson<CubeData>(jsonString);
 
-            for (int i = 0; i < cubeLoader.cubes.Length; i++)
+            if (updatedCube != null)
             {
-                if (cubeLoader.cubes[i].seq == updatedCube.seq)
+                Debug.Log("수정 " + updatedCube.seq + "=" + updatedCube.column5);
+
+                for (int i = 0; i < cubeLoader.cubes.Length; i++)
                 {
-                    cubeLoader.cubes[i] = updatedCube;
-
-                    // 색상 다시 설정
-                    string[] parts = updatedCube.column5.Split('&');
-                    Color colorToUse = Color.gray;
-
-                    if (parts.Length > 1 && int.TryParse(parts[1], out int colorCode))
+                    if (cubeLoader.cubes[i].seq == updatedCube.seq)
                     {
-                        switch (colorCode)
+                        cubeLoader.cubes[i] = updatedCube;
+
+                        // 색상 다시 설정
+                        string[] parts = updatedCube.column5.Split('&');
+                        Color colorToUse = Color.gray;
+
+                        if (parts.Length > 1 && int.TryParse(parts[1], out int colorCode))
                         {
-                            case 1: colorToUse = Color.red; break;
-                            case 2: colorToUse = Color.green; break;
-                            case 3: colorToUse = Color.white; break;
-                            case 4: colorToUse = Color.yellow; break;
-                            case 5: colorToUse = Color.blue; break;
+                            switch (colorCode)
+                            {
+                                case 1: colorToUse = Color.red; break;
+                                case 2: colorToUse = Color.green; break;
+                                case 3: colorToUse = Color.white; break;
+                                case 4: colorToUse = Color.yellow; break;
+                                case 5: colorToUse = Color.blue; break;
+                            }
                         }
+
+                        int batchIndex = i / cubeLoader.batchSize;
+                        int cubeIndexInBatch = i % cubeLoader.batchSize;
+                        MaterialPropertyBlock props = cubeLoader.propertyBatches[batchIndex][cubeIndexInBatch];
+                        if (props == null) props = new MaterialPropertyBlock();
+
+                        props.SetColor("_BaseColor", colorToUse);
+
+                        // 반드시 수정된 MaterialPropertyBlock을 저장 (중요!)
+                        cubeLoader.propertyBatches[batchIndex][cubeIndexInBatch] = props;
+
+
+                        Debug.Log($"Cube {updatedCube.seq} 갱신 완료");
+                        break;
                     }
-
-                    int batchIndex = i / cubeLoader.batchSize;
-                    int cubeIndexInBatch = i % cubeLoader.batchSize;
-                    MaterialPropertyBlock props = new MaterialPropertyBlock();
-                    props.SetColor("_BaseColor", colorToUse);
-                    cubeLoader.propertyBatches[batchIndex][cubeIndexInBatch] = props;
-
-                    Debug.Log($"Cube {updatedCube.seq} 갱신 완료");
-                    break;
                 }
+            }
+            else
+            {
+                Debug.LogError("JSON 변환 실패");
             }
         });
 
