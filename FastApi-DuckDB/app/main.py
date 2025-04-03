3fbb76e8ba40cc2e3f2856bce6e7a33379f2949b
yunji <yunji378mm@naver.com>
2025-04-03 14:15:07 +0900

    feat: delete gzip

--- FastApi-DuckDB/app/main.py
+++ FastApi-DuckDB/app/main.py
@@ -4,7 +4,7 @@ from app.routes import cube_routes
 from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
-app.add_middleware(GZipMiddleware, minimum_size=1000) #g zip사용 속도때매
+# app.add_middleware(GZipMiddleware, minimum_size=1000) #g zip사용 속도때매
 
 app.add_middleware(
     CORSMiddleware,
